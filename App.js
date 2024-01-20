0029bfcc3962e43aefb597700ecc88b1a98b7547
Paul Henstridge <paulhenstridge@gmail.com>
2024-01-20 19:29:46 +0000

    basic scrollable task list

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+
+import { FlatList, StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
+
+const [enteredText, setEnteredText] = useState('');
+const [goals, setGoals] = useState([])
+
+  const goalInputHandler = (text) => {
+    setEnteredText(text)
+  }
+
+  const addGoalHandler = () => {
+    setGoals( prevState => [...prevState, enteredText])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+    <View style={styles.inputContainer}>
+      <TextInput 
+      placeholder='Enter your goal'
+       style={styles.textInput}
+       onChangeText={goalInputHandler}
+       /> 
+      <Button
+       title="Add Goal"
+       onPress={addGoalHandler}
+       />
+    </View>
+
+    
+       <View style={styles.goalsContainer}>
+       <ScrollView>
+      {goals.map((goal, index) => <Text style={styles.goalText} key={index}>{goal}</Text>)}
+      </ScrollView>
+      </View>
+   
+   
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding:50,
+    flex:1,
   },
+  inputContainer: {
+    flexDirection:'row',
+    justifyContent: "space-between",
+    flex:1,
+    borderBottomWidth:1,
+    borderBottomColor:'black'
+  },
+  textInput: {
+    borderWidth:1,
+    borderColor:'black',
+    width:'80%',
+    height:'50%',
+    marginRight:8,
+    padding:8,
+
+  },
+  goalsContainer:{
+    flex:5
+  },
+  goalText:{
+    padding:12,
+    borderRadius:12,
+    borderWidth:2,
+    borderColor:'blue',
+    backgroundColor:'yellow',
+    fontWeight:'bold',
+    margin:6
+  }
 });
